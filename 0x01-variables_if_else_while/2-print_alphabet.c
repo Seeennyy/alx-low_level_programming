65045f2415dd67d34e29b57a686fda941a439ec4
Seeennyy <seeennnyyy@gmail.com>
2023-02-17 00:44:34 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints from a to z
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
