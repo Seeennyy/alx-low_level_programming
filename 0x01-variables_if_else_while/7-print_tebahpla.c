6be4f4de4386f4d0e015d03cdf35a200ee7590ae
Seeennyy <seeennnyyy@gmail.com>
2023-02-17 09:09:33 -0800

    print in reverse mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints from z to a
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char x;
+        for (x = 'z'; x >= 'a'; x--)
+        putchar(x);
+        putchar('\n');
+
+        return (0);
+}
+
