27b5f40d14ade9e5678ff7d6198e3a582eb9e4ac
Seeennyy <seeennnyyy@gmail.com>
2023-02-16 17:00:06 -0800

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - prints "Programming is like building a multilingual puzzle
  * a newline follow
- * Return 0
+ * Return: 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
