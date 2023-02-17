21fb546c08f1964792edc2601514c2241643a3dc
Seeennyy <seeennnyyy@gmail.com>
2023-02-16 18:25:51 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
  * Description: "prints a quote"
  * Retrun Always 1.
  */
-
-	int main(void)
+int main(void)
 {
-
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
-
 }
