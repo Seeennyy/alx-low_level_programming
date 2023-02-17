b2d8bc6612b6442d363626b9e83d772672d3de4f
Seeennyy <seeennnyyy@gmail.com>
2023-02-16 18:20:37 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,14 @@
 
 /**
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
- * Description - printing program
+ * Description: "prints a quote"
  * Retrun Always 1.
  */
 
 int main(void)
-
 {
+
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
+
 }
