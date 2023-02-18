aefb0515d91b4dd3fe9fbae37a8a800a34795419
Seeennyy <seeennnyyy@gmail.com>
2023-02-17 18:53:16 -0800

    done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,24 +8,25 @@
 
 int main(void)
 {
-        int d, p;
-        for (d = '0'; d <= 9; d++)
+	int d, p;
+
+	for (d = '0'; d <= '9'; d++)
 {
-  for (p = d + 1; p <= 9; p++)
+	for (p = d + 1; p <= '9'; p++)
 {
-if (p != d)
+	if (p != d)
 {
-        putchar(d);
-        putchar(d);
-        if (d == '8' && p == 9)
+	putchar(d);	
+	putchar(d);
+if (d == '8' && p == '9')
 continue;
 putchar (',');
 putchar (' ');
-        }
- }
- }
-        putchar('\n');
+}
+}
+}
+putchar('\n');
 
-        return (0);
+return (0);
 }
 
