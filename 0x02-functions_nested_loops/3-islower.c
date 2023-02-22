1a4caf344e9b65be998fc1c2f1929b41418dfdf4
Seeennyy <seeennnyyy@gmail.com>
2023-02-22 07:48:06 -0800

    is lowercase ?

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - Shows 1 when the arg is a lowercase char,
+ * if it is not, it shows 0
+ *
+ * Return: 1 for lowercase char. 0 for the rest
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		retrun (0);
+	}
+	_putchar('\n');
+}
