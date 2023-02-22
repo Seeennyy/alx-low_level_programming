356ad0b021d5a8ea8d2886accacd732630dc13d8
Seeennyy <seeennnyyy@gmail.com>
2023-02-22 08:51:40 -0800

    done

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+/**
+ * jack_bauer - prints the minutes of the day
+ *
+ * Return: nothing
+ */
+
+void jack_bauer(void)
+{
+	int x, y, z, l;
+
+	for (x = 0; x <= 2; x++)
+	{
+		for (y = 0; b <= 9; y++)
+		{
+			for (z = 0; z <= 5; z++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					if (x >= 2 && y >= 4)
+						break;
+					_putchar(x + 48);
+					_putchar(y + 48);
+					_putchar(58);
+					_putchar(z + 48);
+					_putchar(l + 48);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+
+
+}
