ea26efe9aa828aa31b89054bd0ee87f63e1920ca
Seeennyy <seeennnyyy@gmail.com>
2023-02-22 08:20:28 -0800

    done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - computes the abs value
+ *
+ *@r: An integer as an input
+ *
+ * Return: value
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r * -1);
+	}
+
+}
