8c3af50bc962f294be2d5a8c48fdf404ee871f11
Seeennyy <seeennnyyy@gmail.com>
2023-02-22 16:58:20 -0800

    fibo

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main - prints the sum of the last two numbers less 
+ * main - prints the sum of the last two numbers less
  * than 4000000
  * Return: nothing
  */
- 
- int main(void)
+
+int main(void)
 	{
-	int x =0;
+	int x = 0;
 	long y = 1, z = 2, sum = z;
 
 	while (y + z < 4000000)
@@ -15,8 +15,8 @@
 	z += y;
 	if (z % 2 == 0)
 	sum += z;
-	y = z -y;
-       	++x;
+	y = z - y;
+		++x;
 	}
 	printf("%ld\n", sum);
 	return (0);
