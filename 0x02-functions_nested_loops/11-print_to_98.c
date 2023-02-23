ee6cd5d3219c8d1defa8172558b21df9d3a33a06
Seeennyy <seeennnyyy@gmail.com>
2023-02-22 16:35:32 -0800

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,29 +12,29 @@ void print_to_98(int n)
 {
 while (n <= 98)
 {
-    printf("%d", n);
-        if (n != 98)
-        {
-            printf(", ");
-        }
-        n++;
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+n++;
 }
 }
 else if (n > 98)
-	{
-		while (n >= 98)
-    {
-			printf("%d", n);
-			if (n != 98)
-    {
-            printf(", ");
-    }
-            n--;
-    }
-	}
-	else
-	{
-    printf("98");
-	}
-	printf("\n");
+{
+while (n >= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+n--;
+}
+}
+else
+{
+printf("98");
+}
+printf("\n");
 }
