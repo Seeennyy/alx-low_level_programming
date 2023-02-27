0b7536b94fc4bdb03ab56f5ec9e1fef79bfd7ae0
Seeennyy <seeennnyyy@gmail.com>
2023-02-27 12:07:52 -0800

    done

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -20,26 +20,26 @@ int x, y, z, l, m, d;
 		l++;
 
 	while (x < l && m == 0)
-	{
+		{
 		if (s[x] == '-')
 			++y;
 
 		if (s[x] >= '0' && s[x] <= '9')
 		{
 			d = s[x] - '0';
-			if (y % 2)
+		if (y % 2)
 				d = -d;
-			z = z * 10 + digit;
+			z = z * 10 + d;
 			m = 1;
-			if (s[x + 1] < '0' || s[x + 1] > '9')
-				break;
+		if (s[x + 1] < '0' || s[x + 1] > '9')
+			break;
 			m = 0;
 		}
 		x++;
-	}
+		}
 
-	if (m == 0)
-		return (0);
+		if (m == 0)
+	return (0);
 
 	return (z);
 }
